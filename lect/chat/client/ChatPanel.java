0beb5d30a16858e599c4d545b30434af96776e6a
SonMinKyoung <mink@SonMinKyoungui-MacBookPro.local>
2024-01-02 17:27:48 +0900

    RoomMaker1

--- lect/chat/client/ChatPanel.java
+++ lect/chat/client/ChatPanel.java
@@ -24,6 +24,9 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
     ChatRoom room;
     CommandButton connectDisconnect;
     JButton clearChat;
+    // 방 생성 버튼
+    JButton makeRoom;
+
     // 입장 버튼
     JButton enterChat;
     PrintWriter writer;
@@ -38,6 +41,7 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
         connectDisconnect.addActionListener(this);
         clearChat.addActionListener(this);
         enterChat.addActionListener(this);
+        makeRoom.addActionListener(this);
     }
 
     private void initUI() {
@@ -50,11 +54,14 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
         clearChat = new JButton("ClearChat");
         // 입장 버튼
         enterChat = new JButton("EnterChat");
+        // 방생성 버튼
+        makeRoom = new JButton("MakeRoom");
 
         chatTextField.setEnabled(false);
         chatDispArea.setEditable(false);
         clearChat.setEnabled(false);
         enterChat.setEnabled(false);
+        makeRoom.setEnabled(false);
         userList.setEnabled(false);
         roomList.setEnabled(false);
 
@@ -137,6 +144,12 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
         c.gridx = 4;
         c.anchor = GridBagConstraints.CENTER;
         add(enterChat, c);
+
+        c = new GridBagConstraints();
+        c.gridy = 2;
+        c.gridx = 5;
+        c.anchor = GridBagConstraints.CENTER;
+        add(makeRoom, c);
     }
 
     // 서버로부터 받은 응답 처리
@@ -193,7 +206,7 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
             // 챗방 클리어 버튼일때
             if (e.getActionCommand().equals("ClearChat")) {
                 chatDispArea.initDisplay();
-            } else {    //  채팅방 입장 버튼일때
+            } else if (e.getActionCommand().equals("EneterChat"))  {  //  채팅방 입장 버튼일때
                 if (roomList.getSelectedValue() != room) {
                     room = (ChatRoom) roomList.getSelectedValue();
                     if (room == null) {
@@ -212,7 +225,14 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
                 JOptionPane.showMessageDialog(this, "이미 접속해있는 방입니다.", "ChatRoom",
                         JOptionPane.WARNING_MESSAGE);
             }
+            // 채팅방 생성 버튼
+            else{
+
+
+
+            }
         }
+
     }
 
     public void socketClosed() {
@@ -222,6 +242,8 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
         // 입장 버튼 비활성화
         enterChat.setEnabled(false);
         userList.setEnabled(false);
+        //채팅방 생성 버튼 비활성화
+        makeRoom.setEnabled(false);
         // 채팅방 목록 비활성화
         roomList.setEnabled(false);
         // 모든 유저 삭제
@@ -243,6 +265,8 @@ public class ChatPanel extends JPanel implements MessageReceiver, ActionListener
         // 입장 버튼 활성화
         enterChat.setEnabled(true);
         userList.setEnabled(true);
+        //채팅방 생성 활성화
+        makeRoom.setEnabled(true);
         // 채팅방 목록 활성화
         roomList.setEnabled(true);
     }
